0555c9fceae4b749bfec6a9f5ad9d977006cbc38
brs <turtwig@utexas.edu>
2022-11-25 14:12:50 -0600

    printing things properly

---
+++ run.sh
@@ -0,0 +1,1 @@
+_build/install/default/bin/herd7 -set-libdir herd/libdir -show cond -cat herd/libdir/riscv.cat -o out -conf catalogue/riscv/cfgs/finalproj.cfg  catalogue/riscv/tests/lwaqswrlmore.litmus 
