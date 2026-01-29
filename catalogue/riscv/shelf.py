fe693070fe3477e55968283c470bfe9f9d08ce40
Brendan Sweeney <turtwig@utexas.edu>
2026-01-29 11:05:09 -0800

    Adding RISCV catalogue

---
+++ catalogue/riscv/shelf.py
@@ -0,0 +1,41 @@
+record = "RISCV"
+
+cats = [
+    "cats/riscv-defs.cat",
+    "cats/riscv-total.cat",
+    "cats/riscv-tso-defs.cat",
+    "cats/riscv-tso.cat",
+    "cats/riscv.cat",
+    "cats/sc.cat",
+    ]
+
+cfgs = [
+    "cfgs/finalproj.cfg"
+    "cfgs/new-web.cfg"
+]
+
+illustrative_tests = [
+    "tests/SB.litmus",
+    "tests/SB+rfi-pos.litmus",
+    "tests/SB+mfence+po.litmus",
+    "tests/SB+mfences.litmus",
+    "tests/R.litmus",
+    "tests/R+mfence+rfi-po.litmus",
+    "tests/R+mfence+po.litmus",
+    "tests/R+mfences.litmus",
+    "tests/R+po+mfence.litmus",
+    "tests/2+2W.litmus",
+    "tests/2+2W+mfence+po.litmus",
+    "tests/2+2W+mfences.litmus",
+    "tests/LB.litmus",
+    "tests/LB+mfence+po.litmus",
+    "tests/LB+mfences.litmus",
+    "tests/MP.litmus",
+    "tests/MP+mfence+po.litmus",
+    "tests/MP+mfences.litmus",
+    "tests/MP+po+mfence.litmus",
+    "tests/S.litmus",
+    "tests/S+mfence+po.litmus",
+    "tests/S+mfences.litmus",
+    "tests/S+po+mfence.litmus",
+]
